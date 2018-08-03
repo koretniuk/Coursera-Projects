186ab8ec98446b6ecd0fbeaa6e59e3d8749162da
Iurii Koretniuk <yurko.koretniuk@gmail.com>
2018-08-02 21:30:56 -0700

    Add files via upload

---
+++ RollerMaddness/Scripts/ChangeCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Boo.Lang;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+
+	public Camera MainCamera;
+	public Camera SecondaryCamera;
+
+	void Start()
+	{
+		MainCamera.enabled = true;
+		SecondaryCamera.enabled = false;	
+	}
+	
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			SecondaryCamera.enabled = true;	
+			MainCamera.enabled = false;
+		}
+	}
+
+}
+	
